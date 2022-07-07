e808d98ae0879cc5d8e268a5d32f562ed8552226
mclacore <michael@massdriver.cloud>
2022-07-07 10:13:36 -0600

    Deploying cosmos without artifact file

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # azure-cosmosdb-mongo
+
+Azure Cosmos DB is a fully managed, serverless NoSQL database for high-performance applications of any size or scale.
+
+## Development
+
+### Enabling Pre-commit
+
+This repo includes Terraform pre-commit hooks.
+
+[Install precommmit](https://pre-commit.com/index.html#installation) on your system.
+
+```shell
+git init
+pre-commit install
+```
+
+Terraform hooks will now be run on each commit.
+
+### GitHub Action for Publishing to Massdriver
+
+A github workflow for publishing has been configured in `.github/workflows/publish.yaml`.
+
+You'll need to set your `MASSDRIVER_API_KEY` in GitHub secrets.
+
+### Configuring a bundle
+
+`massdriver.yaml` TBD - walk through of fields & purpose (params, connections, artifacts)
+
+### Building a bundle
+
+Variables files can be generated for your bundle from your `massdriver.yaml` file by running
+
+```shell
+mass bundle build
+```
+
+Two development `tfvars` files are provided for setting `params` and `connections`:
+
+```shell
+cd src
+terraform init
+terraform plan -var-file=./dev.connections.tfvars.json -var-file=./dev.params.tfvars.json
+```
+
+### Developing a bundle
+
+`md_metadata` - naming convention prefix, tags etc
+
+### Misc
+
+#### Other files
+* `operator.mdx` TBD
+* `schema.stories.json` TBD
---
+++ src/main.tf
@@ -0,0 +1,94 @@
+locals {
+  # This can only be set to standard, and is required to be set.
+  offer_type = "Standard"
+  kind       = "MongoDB"
+
+  capabilities = {
+    enable_mongo                = "EnableMongo"
+    mongo_version               = "MongoDBv3.4"
+    mongo_enable_doc_level      = "mongoEnableDocLevelTTL"
+    enable_aggregation_pipeline = "EnableAggregationPipeline"
+  }
+  consistency_policy = {
+    # Must be greater than 300 when more than one geo location is used.
+    max_interval_in_seconds = 400
+    # Must be greater than 100000 when more than one geo location is used.
+    max_staleness_prefix = 150000
+  }
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.md_metadata.name_prefix
+  location = var.vnet.specs.azure.region
+}
+
+resource "azurerm_cosmosdb_account" "main" {
+  name                              = var.md_metadata.name_prefix
+  location                          = azurerm_resource_group.main.location
+  resource_group_name               = azurerm_resource_group.main.name
+  offer_type                        = local.offer_type
+  kind                              = local.kind
+  is_virtual_network_filter_enabled = true
+
+  enable_automatic_failover       = var.redundancy.automatic_failover
+  enable_multiple_write_locations = var.redundancy.multi_region_writes
+  mongo_server_version            = var.mongo_server_version
+
+  virtual_network_rule {
+    id = var.vnet.data.infrastructure.default_subnet_id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  capabilities {
+    name = local.capabilities.enable_aggregation_pipeline
+  }
+
+  capabilities {
+    name = local.capabilities.mongo_enable_doc_level
+  }
+
+  capabilities {
+    name = local.capabilities.mongo_version
+  }
+
+  capabilities {
+    name = local.capabilities.enable_mongo
+  }
+
+  consistency_policy {
+    # Mongo only supports strong, bounded staleness, and eventual consistency.
+    consistency_level       = var.consistency_level
+    max_interval_in_seconds = local.consistency_policy.max_interval_in_seconds
+    max_staleness_prefix    = local.consistency_policy.max_staleness_prefix
+  }
+
+  # Primary region - zone redundancy not supported in West US or North Central US currently
+  geo_location {
+    location          = var.redundancy.primary_region.location
+    failover_priority = 0
+    zone_redundant    = var.redundancy.primary_region.zone_redundant
+  }
+
+  # Additional regions - zone redundancy not supported in West US or North Central US currently
+  dynamic "geo_location" {
+    for_each = var.redundancy.additional_regions
+    content {
+      location          = geo_location.value.location
+      failover_priority = geo_location.value.failover_priority
+      zone_redundant    = geo_location.value.zone_redundant
+    }
+  }
+
+  dynamic "backup" {
+    for_each = var.backups.enable_backup ? toset(["enabled"]) : toset([])
+    content {
+      type                = var.backups.backup_type
+      interval_in_minutes = var.backups.interval
+      retention_in_hours  = var.backups.retention
+      storage_redundancy  = var.backups.redundancy
+    }
+  }
+}
