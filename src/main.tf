c80f92682e46a55827cdadbd77cc9a1492c4d6ed
mclacore <michael@massdriver.cloud>
2022-07-19 17:12:01 -0600

    Fixing issues with terraform blocks

--- src/main.tf
+++ src/main.tf
@@ -1,5 +1,4 @@
 locals {
-  # This can only be set to standard, and is required to be set.
   offer_type = "Standard"
   kind       = "MongoDB"
 
@@ -9,17 +8,22 @@ locals {
     mongo_enable_doc_level      = "mongoEnableDocLevelTTL"
     enable_aggregation_pipeline = "EnableAggregationPipeline"
   }
-  consistency_policy = {
-    # Must be greater than 300 when more than one geo location is used.
-    max_interval_in_seconds = 400
-    # Must be greater than 100000 when more than one geo location is used.
-    max_staleness_prefix = 150000
+
+  paired_region_map = {
+    "westus"         = "eastus"
+    "eastus"         = "westus"
+    "westus3"        = "eastus2"
+    "eastus2"        = "westus3"
+    "southcentralus" = "northcentralus"
+    "northcentralus" = "southcentralus"
   }
 }
 
 resource "azurerm_resource_group" "main" {
   name     = var.md_metadata.name_prefix
   location = var.vnet.specs.azure.region
+
+  tags = var.md_metadata.default_tags
 }
 
 resource "azurerm_cosmosdb_account" "main" {
@@ -30,7 +34,7 @@ resource "azurerm_cosmosdb_account" "main" {
   kind                              = local.kind
   is_virtual_network_filter_enabled = true
 
-  enable_automatic_failover       = var.redundancy.automatic_failover
+  enable_automatic_failover       = var.geo_redundancy.automatic_failover
   enable_multiple_write_locations = var.database.multi_region_writes
   mongo_server_version            = var.database.mongo_server_version
 
@@ -70,32 +74,38 @@ resource "azurerm_cosmosdb_account" "main" {
   }
 
   consistency_policy {
-    # Mongo only supports strong, bounded staleness, and eventual consistency.
     consistency_level       = var.database.consistency_level
-    max_interval_in_seconds = local.consistency_policy.max_interval_in_seconds
-    max_staleness_prefix    = local.consistency_policy.max_staleness_prefix
+    max_interval_in_seconds = var.database.max_interval_in_seconds
+    max_staleness_prefix    = var.database.max_staleness_prefix
   }
 
-  # Primary region - zone redundancy not supported in West US or North Central US currently
   geo_location {
-    location          = var.redundancy.primary_region.location
+    location          = azurerm_resource_group.main.location
     failover_priority = 0
-    zone_redundant    = var.redundancy.primary_region.zone_redundant
   }
 
-  # Additional regions - zone redundancy not supported in West US or North Central US currently
+  geo_location {
+    location          = local.paired_region_map[azurerm_resource_group.main.location]
+    failover_priority = 1
+  }
+
   dynamic "geo_location" {
-    for_each = var.redundancy.additional_regions
+    for_each = { for ar in var.geo_redundancy.additional_regions : ar.location => ar }
     content {
       location          = geo_location.value.location
       failover_priority = geo_location.value.failover_priority
-      zone_redundant    = geo_location.value.zone_redundant
     }
   }
 
-  # Need to solve for continuous backup setting
   dynamic "backup" {
-    for_each = var.backups.enable_backup ? toset(["enabled"]) : toset([])
+    for_each = var.backups.backup_type == "Continuous" ? toset(["Continuous"]) : toset([])
+    content {
+      type = var.backups.backup_type
+    }
+  }
+
+  dynamic "backup" {
+    for_each = var.backups.backup_type == "Periodic" ? toset(["Periodic"]) : toset([])
     content {
       type                = var.backups.backup_type
       interval_in_minutes = var.backups.interval
@@ -103,4 +113,6 @@ resource "azurerm_cosmosdb_account" "main" {
       storage_redundancy  = var.backups.redundancy
     }
   }
+
+  tags = var.md_metadata.default_tags
 }
