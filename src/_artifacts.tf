14d9d7224a042cfe267810a05d23f80387d2a568
mclacore <michael@massdriver.cloud>
2022-07-21 17:33:36 -0600

    Artifact for Cosmos DB Mongo

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,32 +1,30 @@
-# locals {
-#   data_infrastructure = {
-#     ari = azurerm_cosmosdb_account.main.id
-#   }
-#   data_authentication = {
-#     username = "${var.md_metadata.name_prefix}"
-#     password = azurerm_cosmosdb_account.main.primary_key
-#     hostname = "${var.md_metadata.name_prefix}.mongo.cosmos.azure.com"
-#     port     = 10255
-#   }
-#   data_security = {}
-# }
+locals {
+  data_infrastructure = {
+    ari = azurerm_cosmosdb_account.main.id
+  }
+  data_authentication = {
+    username = "${var.md_metadata.name_prefix}"
+    password = azurerm_cosmosdb_account.main.primary_key
+    hostname = "${var.md_metadata.name_prefix}.mongo.cosmos.azure.com"
+    port     = 10255
+  }
+}
 
-# resource "massdriver_artifact" "authentication" {
-#   field                = "authentication"
-#   provider_resource_id = azurerm_cosmosdb_account.main.id
-#   name                 = "CosmosDB Server ${var.md_metadata.name_prefix} (${azurerm_cosmosdb_account.main.id})"
-#   artifact = jsonencode(
-#     {
-#       data = {
-#         infrastructure = local.data_infrastructure
-#         authentication = local.data_authentication
-#         security       = local.data_security
-#       }
-#       specs = {
-#         azure = {
-#           region = azurerm_resource_group.main.location
-#         }
-#       }
-#     }
-#   )
-# }
+resource "massdriver_artifact" "authentication" {
+  field                = "mongo_authentication"
+  provider_resource_id = azurerm_cosmosdb_account.main.id
+  name                 = "CosmosDB Server ${var.md_metadata.name_prefix} (${azurerm_cosmosdb_account.main.id})"
+  artifact = jsonencode(
+    {
+      data = {
+        infrastructure = local.data_infrastructure
+        authentication = local.data_authentication
+      }
+      specs = {
+        mongo = {
+          version = azurerm_cosmosdb_account.main.mongo_server_version
+        }
+      }
+    }
+  )
+}
