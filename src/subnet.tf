5e39dbd81882d4025114e47acf2269e3ec8d8298
mclacore <michael@massdriver.cloud>
2022-11-23 12:41:00 -0700

    Auto CIDR feature

--- src/subnet.tf
+++ src/subnet.tf
@@ -2,12 +2,31 @@ locals {
   split_vnet_id       = split("/", var.azure_virtual_network.data.infrastructure.id)
   vnet_name           = element(local.split_vnet_id, length(local.split_vnet_id) - 1)
   vnet_resource_group = element(local.split_vnet_id, index(local.split_vnet_id, "resourceGroups") + 1)
+  cidr                = var.network.auto ? utility_available_cidr.cidr.result : var.network.cidr
+}
+
+data "azurerm_virtual_network" "lookup" {
+  name                = local.vnet_name
+  resource_group_name = local.vnet_resource_group
+}
+
+data "azurerm_subnet" "lookup" {
+  for_each             = toset(data.azurerm_virtual_network.lookup.subnets)
+  name                 = each.key
+  virtual_network_name = local.vnet_name
+  resource_group_name  = local.vnet_resource_group
+}
+
+resource "utility_available_cidr" "cidr" {
+  from_cidrs = data.azurerm_virtual_network.lookup.address_space
+  used_cidrs = flatten([for subnet in data.azurerm_subnet.lookup : subnet.address_prefixes])
+  mask       = 28
 }
 
 resource "azurerm_subnet" "main" {
   name                 = var.md_metadata.name_prefix
   resource_group_name  = local.vnet_resource_group
   virtual_network_name = local.vnet_name
-  address_prefixes     = [var.database.cidr]
+  address_prefixes     = [local.cidr]
   service_endpoints    = ["Microsoft.AzureCosmosDB"]
 }
