b474fbb8ec8616720a8f8257b6603164d84c94b6
mclacore <michael@massdriver.cloud>
2023-09-14 15:44:52 -0700

    Verified fix for mongo network issue

--- src/subnet.tf
+++ src/subnet.tf
@@ -30,3 +30,22 @@ resource "azurerm_subnet" "main" {
   address_prefixes     = [local.cidr]
   service_endpoints    = ["Microsoft.AzureCosmosDB"]
 }
+
+resource "azurerm_private_endpoint" "default" {
+  name                = "${var.md_metadata.name_prefix}-pe"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_service_connection {
+    name                           = "${var.md_metadata.name_prefix}-psc"
+    private_connection_resource_id = azurerm_cosmosdb_account.main.id
+    subresource_names              = ["MongoDB"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "privatelink_mongo_cosmos_azure_com"
+    private_dns_zone_ids = ["/subscriptions/${var.azure_service_principal.data.subscription_id}/resourceGroups/${local.vnet_resource_group}/providers/Microsoft.Network/privateDnsZones/privatelink.mongo.cosmos.azure.com"]
+  }
+}
